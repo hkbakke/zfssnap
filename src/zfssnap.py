3e27a9e19890cfae0b4550e911805a962f5ed002
hkbakke <hkbakke@users.noreply.github.com>
2017-01-11 19:12:04 +0100

    Bump version to 3.5.0

--- src/zfssnap.py
+++ src/zfssnap.py
@@ -24,7 +24,7 @@ except ImportError:
     from scandir import scandir
 
 
-VERSION = '3.4.0'
+VERSION = '3.5.0'
 PROPERTY_PREFIX = 'zfssnap'
 ZFSSNAP_LABEL = '%s:label' % PROPERTY_PREFIX
 ZFSSNAP_REPL_STATUS = '%s:repl_status' % PROPERTY_PREFIX
