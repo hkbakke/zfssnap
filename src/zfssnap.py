34b8cfeb1d9722ed464c92b0da2be2a0fa0912d9
Hans-Kristian Bakke <hkbakke@gmail.com>
2017-02-02 10:53:54 +0100

    Bump version

--- src/zfssnap.py
+++ src/zfssnap.py
@@ -25,7 +25,7 @@ except ImportError:
     from scandir import scandir
 
 
-VERSION = '3.6.2'
+VERSION = '3.6.3'
 PROPERTY_PREFIX = 'zfssnap'
 ZFSSNAP_LABEL = '%s:label' % PROPERTY_PREFIX
 ZFSSNAP_REPL_STATUS = '%s:repl_status' % PROPERTY_PREFIX
