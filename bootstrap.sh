327fc5c554af76430243fa21ad212c88431e698d
hkbakke <hkbakke@users.noreply.github.com>
2017-04-20 08:49:44 +0200

    Update bootstrap.sh

--- bootstrap.sh
+++ bootstrap.sh
@@ -12,6 +12,7 @@ zpool create -f -m /zpools/dev-1 dev-1 mirror /dev/sdb /dev/sdc
 zfs set compression=lz4 dev-1
 zfs set xattr=sa dev-1
 zfs set acltype=posixacl dev-1
+zfs set atime=off dev-1
 zfs create dev-1/test-1
 zfs create dev-1/test-2
 zfs create dev-1/test-3
@@ -19,6 +20,7 @@ zpool create -f -m /zpools/dev-2 dev-2 mirror /dev/sdd /dev/sde
 zfs set compression=lz4 dev-2
 zfs set xattr=sa dev-2
 zfs set acltype=posixacl dev-2
+zfs set atime=off dev-2
 zfs create dev-2/backup
 
 pip3 install pytest pytest-cov scandir pylint python-dateutil
